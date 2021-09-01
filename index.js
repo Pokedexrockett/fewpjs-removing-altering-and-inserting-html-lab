1557373f800ae50e3e0b7121cfffaeb22f33e557
Dexter <dex429.dc@gmail.com>
2021-09-01 17:47:53 -0400

    commit tree 48e303652b35a411646c6a5f51338df94b7f8f5f

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code here!
+let node = document.getElementById("main");
+if (node.parentNode) {
+  node.parentNode.removeChild(node);
+}
+
+let newHeader = document.querySelector("h1")
